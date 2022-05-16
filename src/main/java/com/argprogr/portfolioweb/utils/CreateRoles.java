d4d1eabc0324ec897e738df79523040893c2889c
Esteban <estebandpisani@gmail.com>
2022-05-16 16:53:11 -0300

    deploy heroku

--- src/main/java/com/argprogr/portfolioweb/utils/CreateRoles.java
+++ src/main/java/com/argprogr/portfolioweb/utils/CreateRoles.java
@@ -35,30 +35,29 @@ public class CreateRoles implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
     	
-    	Scanner scan = new Scanner(System.in);
-    	/*
-        System.out.println("Creando roles...");    	
-        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-        Rol rolUser = new Rol(RolNombre.ROLE_USER);
-        rolService.save(rolAdmin);
-        rolService.save(rolUser);
-        System.out.println("Roles creados..."); 
-        */  
-    	/*
-        System.out.println("Creando usuario..."); 
-        System.out.println("Ingrese el username");
-        String username = scan.next();
-        System.out.println("Ingrese el password");        
-        String password = scan.next();    
+//    	Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Creando roles...");    	
+//        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+//        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+//        rolService.save(rolAdmin);
+//        rolService.save(rolUser);
+//        System.out.println("Roles creados..."); 
+//
+//        System.out.println("Creando usuario..."); 
+//        System.out.println("Ingrese el username");
+//        String username = scan.next();
+//        System.out.println("Ingrese el password");        
+//        String password = scan.next();    
+//        
+//        Usuario usuario = new Usuario(username, encoder.encode(password));
+//        System.out.println("Asignando roles...");
+//        Set<Rol> roles = new HashSet<>();
+//        roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+//        roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+//        usuario.setRoles(roles);
+//        System.out.println("Guardando usuario...");
+//        usuarioService.saveUsuario(usuario);
         
-        Usuario usuario = new Usuario(username, encoder.encode(password));
-        System.out.println("Asignando roles...");
-        Set<Rol> roles = new HashSet<>();
-        roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
-        roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
-        usuario.setRoles(roles);
-        System.out.println("Guardando usuario...");
-        usuarioService.saveUsuario(usuario);
-         */
     }
 }
